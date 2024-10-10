9a4816fceeb8a622feb231cd4cc8db47febe867d
Ivan Gunchenko <jgunchenko@gmail.com>
2024-10-10 13:57:03 +0200

    display in alert

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
       </div>
       {/*  */}
       <div className="card">
-        <button onClick={() => WebApp.showAlert(`Hello World! Current count is ${count}`)}>
+        <button onClick={() => WebApp.showAlert(`${JSON.stringify(WebApp)}`)}>
             Show Alert
         </button>
       </div>
