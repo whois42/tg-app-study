b866cff9e9782be7a335d2a5bd6ef9076d4121e8
Ivan Gunchenko <jgunchenko@gmail.com>
2024-10-10 11:31:48 +0200

    remove obsolete

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import twaLogo from './assets/tapps.png'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 import WebApp from '@twa-dev/sdk'
@@ -11,17 +8,6 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://ton.org/dev" target="_blank">
-          <img src={twaLogo} className="logo" alt="TWA logo" />
-        </a>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>TWA + Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
