a60dc3346673fd80ec0941c667950af72e7cd2bd
Ivan Gunchenko <jgunchenko@gmail.com>
2024-10-17 13:45:04 +0200

    use user data

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,31 @@
 import './App.css'
 
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { WebAppUser } from '@twa-dev/types'; // Add this line to import WebAppUser type
 import WebApp from '@twa-dev/sdk'
 import { MainButton, BottomBar } from '@twa-dev/sdk/react';
 import {ProfileForm} from "./components/ProfileForm";
 
 function App() {
-  // const [count, setCount] = useState(0)
+  const [user, setUser] = useState<WebAppUser & { added_to_attachment_menu?: boolean; allows_write_to_pm?: boolean } | null>(null)
   useEffect(() => {
-    console.log(WebApp);
+    if(WebApp.initDataUnsafe.user){
+      console.log(WebApp.initDataUnsafe.user);
+      setUser(WebApp.initDataUnsafe.user)
+    }
     
   }, [])
 
   const handleClick = () => {
     WebApp.showAlert(`AAAA`)
-    if(WebApp.initDataUnsafe.user){
-      console.log(WebApp.initDataUnsafe.user);
-      
-    }
+
     
   }
 
   return (
     <>
       <h1>Open headliner</h1>
-      <ProfileForm/>
+      <ProfileForm user={user}/>
       <div className="card">
         <MainButton
           text="Open headliner"
--- src/components/ProfileForm.tsx
+++ src/components/ProfileForm.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import * as Form from '@radix-ui/react-form';
 
-export const ProfileForm: React.FC = () => {
-    return (
+export const ProfileForm: React.FC = ({user}) => {
+    return user? (
+        <div>
+            <div>
+            <img src={user.photo_url} alt="profile picture"/>
+            <span>{user.username}</span>
+            </div>
+            
         <Form.Root>
             <Form.Field name="fullName">
                 <Form.Label>Full Name</Form.Label>
                 <Form.Control asChild>
-                    <input type="text" required />
+                    <input type="text" required placeholder={`${user.first_name} ${user.last_name}`}/>
                 </Form.Control>
             </Form.Field>
 
@@ -29,5 +35,6 @@ export const ProfileForm: React.FC = () => {
                 <button type="submit">Submit</button>
             </Form.Submit>
         </Form.Root>
-    );
+        </div>
+    ): null;
 };
