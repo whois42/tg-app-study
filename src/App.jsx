d2a6d7b43ecdc071833309f241adbafca068a32e
Ivan Gunchenko <jgunchenko@gmail.com>
2024-10-28 15:05:08 +0100

    wip

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,17 @@ import './App.css'
 import { useEffect, useState } from 'react';
 // import {BrowserRouter, Route, Routes} from "react-router-dom";
 
-import { WebAppUser } from '@twa-dev/types';
+// import { WebAppUser } from '@twa-dev/types';
 import WebApp from '@twa-dev/sdk'
 import { MainButton, BottomBar } from '@twa-dev/sdk/react';
 import {ProfileForm} from "./components/ProfileForm";
 import {telegramLogin} from "./services/auth";
 import {getEvents} from "./services/events";
 
-type User = WebAppUser & { added_to_attachment_menu?: boolean; allows_write_to_pm?: boolean } | null
+// type User = WebAppUser & { added_to_attachment_menu?: boolean; allows_write_to_pm?: boolean } | null
 
 function App() {
-  const [user, setUser] = useState<User>(null)
+  const [user, setUser] = useState(null)
   const [events, setEvents] = useState([]);
   useEffect(() => {
     handleTelegramLogin()
