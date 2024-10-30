52d42d446cdc1ec099dd9e857a74c5aa7674139a
Ivan Gunchenko <jgunchenko@gmail.com>
2024-10-30 12:53:30 +0100

    wip

--- src/services/axiosInstance.js
+++ src/services/axiosInstance.js
@@ -2,13 +2,14 @@ import axios from 'axios';
 import { getToken } from './auth';
 
 const instance = axios.create({
-  baseURL: 'https://acb8-87-212-202-77.ngrok-free.app',
+  baseURL: 'https://2381-87-212-202-77.ngrok-free.app',
 });
 
 instance.interceptors.request.use((config) => {
   const token = getToken();
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
+    config.headers["ngrok-skip-browser-warning"] = "true";
   }
   return config;
 });
