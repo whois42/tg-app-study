b8e781be3657b3d4df04d6ecfca669bf140097e8
Ivan Gunchenko <jgunchenko@gmail.com>
2024-10-30 14:08:00 +0100

    wip

--- src/services/user.js
+++ src/services/user.js
@@ -6,7 +6,7 @@ export async function getSelf() {
 }
 
 export async function createUser(userData) {
-    const response = await axiosInstance.post('/users/', userData);
+    const response = await axiosInstance.post('/users/users/', userData);
     return response.data;
   }
 
