c0b9d128f35e296b8a9c11fb7d71b6ef9856e45f
Ivan Gunchenko <jgunchenko@gmail.com>
2024-10-31 12:19:49 +0100

    wip

--- src/components/ProfileForm.tsx
+++ src/components/ProfileForm.tsx
@@ -1,9 +1,49 @@
 import { WebAppUser } from '@twa-dev/types';
 import { Avatar, Input,} from '@telegram-apps/telegram-ui';
+import { MainButton } from '@twa-dev/sdk/react';
+import { useState } from 'react';
 
-type User = WebAppUser & { added_to_attachment_menu?: boolean; allows_write_to_pm?: boolean } | null
-export const ProfileForm = ({user}:{user: User}) => {
-    console.log(user);
+type UserProfile = {
+    username: string;
+    first_name: string;
+    last_name: string;
+    company: string;
+    position: string;
+}
+type ProfileFormProps = {
+    user: WebAppUser | null;
+    onSubmit: (user: UserProfile) => void;
+}
+
+// type User = WebAppUser & { added_to_attachment_menu?: boolean; allows_write_to_pm?: boolean } | null
+export const ProfileForm = ({user, onSubmit}:ProfileFormProps) => {
+    const [first_name, setFirstName] = useState(user?.first_name || "");
+    const [last_name, setLastName] = useState(user?.last_name || "");
+    const [company, setCompany] = useState("");
+    const [position, setPosition] = useState("");
+
+    const handleFirstNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setFirstName(e.target.value);
+    }
+    const handleLastNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setLastName(e.target.value);
+    }
+    const handleCompanyChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setCompany(e.target.value);
+    }
+    const handlePositionChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setPosition(e.target.value);
+    }
+
+    const handleSubmit = () => {
+        onSubmit({
+            username: user?.username || "",
+            first_name,
+            last_name,
+            company,
+            position,
+        });
+    }
     return user? (
         <div>
             <Avatar
@@ -16,24 +56,29 @@ export const ProfileForm = ({user}:{user: User}) => {
                 <Input
                     header="First name"
                     value={user.first_name}
-                    onChange={(e) => console.log(e.target.value)}
+                    onChange={handleFirstNameChange}
                 />
                 <Input
                     header="Last name"
                     value={user.last_name}
-                    onChange={(e) => console.log(e.target.value)}
+                    onChange={handleLastNameChange}
                 />
                 <Input
                     header="Company"
                     value={user.last_name}
-                    onChange={(e) => console.log(e.target.value)}
+                    onChange={handleCompanyChange}
                 />
                 <Input
                     header="Position"
                     value={user.last_name}
-                    onChange={(e) => console.log(e.target.value)}
+                    onChange={handlePositionChange}
                 />
             </div>
+                    <MainButton
+          text="Submit"
+          onClick={handleSubmit}
+        />
+            
         </div>
     ): null;
 };
