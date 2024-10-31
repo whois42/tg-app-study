bfd5c454af040f58008a3eb095c083a568646036
Ivan Gunchenko <jgunchenko@gmail.com>
2024-10-31 12:35:41 +0100

    wip

---
+++ src/screens/Registration.jsx
@@ -0,0 +1,23 @@
+// import { WebAppUser } from '@twa-dev/types';
+import {createUser} from "../services/user.js";
+import { ProfileForm } from '../components/ProfileForm';
+
+// type UserProfile = {
+//     username: string;
+//     first_name: string;
+//     last_name: string;
+//     company: string;
+//     position: string;
+// }
+// type ProfileFormProps = {
+//     user: WebAppUser | null;
+// }
+
+// type User = WebAppUser & { added_to_attachment_menu?: boolean; allows_write_to_pm?: boolean } | null
+export const RegistrationForm = ({user}) => {
+
+    const handleSubmit = async (userData) => {
+        await createUser(userData);
+    }
+    return <ProfileForm user={user} handleSubmit={handleSubmit}/>
+};
