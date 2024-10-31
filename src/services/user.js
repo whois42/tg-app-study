89d0597e4ccd0576b0bc4e85bec970250ba8258d
Ivan Gunchenko <jgunchenko@gmail.com>
2024-10-31 15:56:14 +0100

    wip

--- src/services/user.js
+++ src/services/user.js
@@ -5,15 +5,7 @@ export async function getSelf() {
   return response.data;
 }
 
-export async function createUser(telegramData) {
-    console.log(telegramData, "telegram data");
-    
-    const userData = {
-        username: telegramData.username,
-        first_name: telegramData.first_name,
-        last_name: telegramData.last_name,
-        telegram_id: telegramData.id,
-    }
+export async function createUser(userData) {
     console.log(userData, "user data");
     
     const response = await axiosInstance.post('/users/init/', userData);
