9521093e9fe162fcd8585fa56624f2cfbfe63978
Ivan Gunchenko <jgunchenko@gmail.com>
2024-11-01 20:13:43 +0100

    wip

--- src/screens/Layout.tsx
+++ src/screens/Layout.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { useLocation, useNavigate } from "react-router-dom";
+import { useLocation, useNavigate, Outlet } from "react-router-dom";
+// import { Outlet, Link } from 'react-router-dom';
 import { Tabbar } from "@telegram-apps/telegram-ui";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
@@ -16,7 +17,7 @@ export const Layout = ({children}: {children: React.ReactNode}) => {
 
     return (
         <div>
-            {children}
+            <Outlet />
             <Tabbar>
                 {tabs.map(({ name, route, Icon }) => (
                     <Tabbar.Item key={name} text={name} selected={location.pathname === route} onClick={() => handleTabClick(route)}>
