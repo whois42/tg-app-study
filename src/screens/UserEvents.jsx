eaa8f57eb33516d29109b86d2e094a0896bb4889
Ivan Gunchenko <jgunchenko@gmail.com>
2024-11-05 09:21:49 +0100

    wip

--- src/screens/UserEvents.jsx
+++ src/screens/UserEvents.jsx
@@ -3,6 +3,7 @@ import { EventList } from "../components/EventList";
 import { useEffect, useState } from "react";
 
 export const UserEventsScreen = () => {
+    console.log("UserrScreen");
     const [events, setEvents] = useState([]);
     const getEventsData = async () => {
         const eventsData = await getEvents();
