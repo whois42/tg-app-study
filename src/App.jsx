a7fa5f735bada005d92684aab4ee9142a17a9c0a
Ivan Gunchenko <jgunchenko@gmail.com>
2024-11-08 11:10:19 +0100

    wip

--- src/App.jsx
+++ src/App.jsx
@@ -83,7 +83,7 @@ function App() {
     <HashRouter>
       <Routes>
         <Route path="/"  element={<Layout />}>
-          <Route path="*" element={!isFirstVisit ? <Navigate to="/discover" /> : <Navigate to="/register" />}/>
+          {/* <Route path="*" element={!isFirstVisit ? <Navigate to="/discover" /> : <Navigate to="/register" />}/> */}
           <Route path="/register" element={<RegistrationScreen user={user} />} />
           <Route path="/discover" element={<DiscoverScreen />} />
           <Route path="/create-event" element={<CreateEventScreen />} />
