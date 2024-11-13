2c13b59291647c7511452a9e48280cbb4ab1c903
Ivan Gunchenko <jgunchenko@gmail.com>
2024-11-13 09:44:22 +0100

    wip

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,23 @@
-import './App.css'
+import './App.css';
 import { useLaunchParams, miniApp, useSignal } from '@telegram-apps/sdk-react';
 import { AppRoot } from '@telegram-apps/telegram-ui';
 import { useEffect, useState } from 'react';
-import WebApp from '@twa-dev/sdk'
-import {RegistrationScreen} from "./screens/Registration.jsx";
-import {CreateEventScreen} from "./screens/CreateEvent.jsx";
-import {Layout} from "./screens/Layout.tsx";
-import {DiscoverScreen} from "./screens/Discover.jsx";
-import {UserEventsScreen} from "./screens/UserEvents.jsx";
+import WebApp from '@twa-dev/sdk';
+import { RegistrationScreen } from "./screens/Registration.jsx";
+import { CreateEventScreen } from "./screens/CreateEvent.jsx";
+import { Layout } from "./screens/Layout.tsx";
+import { DiscoverScreen } from "./screens/Discover.jsx";
+import { UserEventsScreen } from "./screens/UserEvents.jsx";
 
-import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
-import {getSelf} from "./services/user";
-import {telegramLogin} from "./services/auth";
-
+import { HashRouter, Routes, Route, useNavigate } from 'react-router-dom';
+import { getSelf } from "./services/user";
+import { telegramLogin } from "./services/auth";
 
 function App() {
-  const [user, setUser] = useState(null)
+  const [user, setUser] = useState(null);
   const [isFirstVisit, setIsFirstVisit] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
+  const navigate = useNavigate();
 
   const handleTelegramLogin = async () => {
     const telegramData = WebApp.initDataUnsafe || {}; // Use Telegram data
@@ -31,7 +31,6 @@ function App() {
           setUser(user);
         } catch (error) {
           if (error.response && error.response.status === 404) {
-            // User not found, create a new user
             console.log("User not found, creating a new user");
             setIsFirstVisit(true);
           } else {
@@ -41,46 +40,51 @@ function App() {
       } catch (error) {
         console.error("Telegram login failed:", error);
       } finally {
-        console.log("Finally");
         setIsLoading(false);
       }
     } else {
       setIsLoading(false);
     }
   };
-  
+
   useEffect(() => {
     WebApp.ready();
     handleTelegramLogin();
   }, []);
 
+  // Redirect based on login status and first visit
+  useEffect(() => {
+    if (!isLoading) {
+      if (isFirstVisit) {
+        navigate('/register');
+      } else {
+        navigate('/events/discover');
+      }
+    }
+  }, [isFirstVisit, isLoading, navigate]);
+
   const lp = useLaunchParams();
   const isDark = useSignal(miniApp.isDark);
-  console.log(lp, "launch params");
-  console.log(isFirstVisit, "isFirstVisit");
-  
-  
-
 
   return (
     <AppRoot
-    appearance={isDark ? 'dark' : 'light'}
-    platform={['macos', 'ios'].includes(lp.platform) ? 'ios' : 'base'}
-  >
-{  isLoading ?  <div>Loading</div> :
-    <HashRouter>
-      <Routes>
-        <Route path="/" element={!isFirstVisit ? <Navigate to="/events/discover" /> : <Navigate to="/register" />} />
-        <Route path="/register" element={<RegistrationScreen user={user} />} />
-        <Route path="/events" element={<Layout />}>
-          <Route path="discover" element={<DiscoverScreen />} />
-          <Route path="create-event" element={<CreateEventScreen />} />
-          <Route path="my-events" element={<UserEventsScreen />} />
-        </Route>
-      </Routes>
-    </HashRouter>}
+      appearance={isDark ? 'dark' : 'light'}
+      platform={['macos', 'ios'].includes(lp.platform) ? 'ios' : 'base'}
+    >
+      {isLoading ? <div>Loading</div> : (
+        <HashRouter>
+          <Routes>
+            <Route path="/register" element={<RegistrationScreen user={user} />} />
+            <Route path="/events" element={<Layout />}>
+              <Route path="discover" element={<DiscoverScreen />} />
+              <Route path="create-event" element={<CreateEventScreen />} />
+              <Route path="my-events" element={<UserEventsScreen />} />
+            </Route>
+          </Routes>
+        </HashRouter>
+      )}
     </AppRoot>
-  )
+  );
 }
 
-export default App
+export default App;
