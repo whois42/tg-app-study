b358bf44d80abfbb90abec820f2d4c0169af901a
Ivan Gunchenko <jgunchenko@gmail.com>
2024-11-13 18:39:16 +0100

    wip

--- src/context/user.tsx
+++ src/context/user.tsx
@@ -1,4 +1,4 @@
-import { createContext, useState, ReactNode } from 'react';
+import { createContext, useState, ReactNode, useContext } from 'react';
 
 type User = {
     id: number,
@@ -8,13 +8,19 @@ type User = {
     telegram_id: string
 }
 
-export const UserContext = createContext<{ user: User | null, updateUser: (userData: User) => void } | null>({user: null, updateUser: () => {}});
+type UserContextType = {
+    user: User | null;
+    updateUser: (userData: User) => void;
+};
+const UserContext = createContext<UserContextType|null>(null); // Use appropriate types if necessary
+
+export const useUser = () => useContext(UserContext);
 
 export const UserProvider = ({ children }: { children: ReactNode }) => {
   const [user, setUser] = useState<User | null>(null);
 
   // Define updateUser explicitly with a parameter
-  const updateUser = (userData: User) => {
+  const updateUser = (userData: User) => {  // Replace `any` with the appropriate type if available
     setUser(userData);
   };
 
